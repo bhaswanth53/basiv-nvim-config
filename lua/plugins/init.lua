54593b2ed9135e82a3ab1ea025733f38117be4f8
Bhaswanth Chiruthanuru <bhaswanth22@gmail.com>
2025-07-15 11:13:59 +0530

    Modified structure

---
+++ lua/plugins/init.lua
@@ -0,0 +1,28 @@
+return {
+    -- Editor plugins
+    require("plugins.editor.alpha"),
+    require("plugins.editor.bufferline"),
+    require("plugins.editor.lualine"),
+    require("plugins.editor.neo-tree"),
+    require("plugins.editor.noice"),
+    require("plugins.editor.telescope"),
+
+    -- Coding plugins
+    require("plugins.coding.autopairs"),
+    require("plugins.coding.completions"),
+    require("plugins.coding.indent-blankline"),
+    require("plugins.coding.treesitter"),
+
+    -- Lsp
+    require("plugins.lsp.lsp-config"),
+    require("plugins.lsp.none-ls"),
+
+    -- Themes
+    require("plugins.themes.catppuccin"),
+    require("plugins.themes.monokai-pro"),
+    require("plugins.themes.one-monokai"),
+    require("plugins.themes.onedarkpro"),
+    require("plugins.themes.space"),
+    require("plugins.themes.vscode"),
+    require("plugins.themes.zephyr"),
+}
